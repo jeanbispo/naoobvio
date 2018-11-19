ed334cb7b3dffc281e5cc2ddbb9d86551f62860a
Jean Bispo <jeanbispo@hotmail.com>
2018-11-19 11:03:37 -0300

    Update playbuzzresize.js

--- js/playbuzzresize.js
+++ js/playbuzzresize.js
@@ -1,8 +1,17 @@
 jQuery(document).ready(function($){
-window.addEventListener('load', function() {
-    		 var contents = $('.playbuzz iframe').contents();
-styleTag = $('<style>html, body, .pb-story{max-width: 100% !important;}</style>').appendTo(contents.find('head'));
-})
+	var e = 0;
+	while(e != 1){
+	
+		window.addEventListener('load', function() {
+    		var contents = $('.playbuzz iframe').contents();
+		styleTag = $('<style>html, body, .pb-story{max-width: 100% !important;}</style>').appendTo(contents.find('head'));
+		})
+		if($(".new-item-stats").hasClass("embed-stat")){
+		e = 1;
+		}
+		
+	}
+
 });
 	
 	 
