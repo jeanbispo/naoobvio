1379a19cf8637246172cea7b9836957cfc2fec57
Jean Bispo <jeanbispo@hotmail.com>
2018-11-19 10:44:14 -0300

    Update playbuzzresize.js

--- js/playbuzzresize.js
+++ js/playbuzzresize.js
@@ -1,6 +1,8 @@
+jQuery(document).ready(function($){
 window.addEventListener('load', function() {
     		 var contents = $('.playbuzz iframe').contents();
 styleTag = $('<style>html, body, .pb-story{max-width: 100% !important;}</style>').appendTo(contents.find('head'));
 })
+});
 	
 	 
